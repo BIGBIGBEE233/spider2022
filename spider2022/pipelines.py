9606d30db5404b3c078aa80dfa7834040f4ba7ae
BIGBEE <316182952@qq.com>
2022-08-23 10:58:39 +0800

    add batch processing function

--- spider2022/pipelines.py
+++ spider2022/pipelines.py
@@ -14,20 +14,39 @@ class DbPipeline:
         self.conn = pymysql.connect(host='localhost', port=3306, user='root', password='lxLR0227',
                                     database='spider_db', charset='utf8mb4')
         self.cursor = self.conn.cursor()
+        self.data=[]
 
     def process_item(self, item, spider):
         title = item.get('title', '')
         score = item.get('score', '')
         subject = item.get('subject', '')
-        self.cursor.execute(
-            'insert into douban_movie_top250 (movie_title,movie_score,movie_subject) values (%s,%s,%s)', (title, score, subject)
-        )
+
+        # 每次获取到数据立即写入
+        # self.cursor.execute(
+        #     'insert into douban_movie_top250 (movie_title,movie_score,movie_subject) values (%s,%s,%s)',
+        #     (title, score, subject)
+        # )
+
+        # 数据批量写入数据库
+        self.data.append((title,score,subject))
+        if len(self.data)==50:
+            self._write_data_to_db()
+            self.data.clear()
 
         return item
 
-    def close_spider(self, spider):
+    def _write_data_to_db(self):
+        self.cursor.executemany(
+            'insert into douban_movie_top250 (movie_title,movie_score,movie_subject) values (%s,%s,%s)',
+            self.data
+        )
         self.conn.commit()
+
+    def close_spider(self, spider):
+        if len(self.data)>0:
+            self._write_data_to_db()
         self.conn.close()
+
 
 
 class Spider2022Pipeline:
